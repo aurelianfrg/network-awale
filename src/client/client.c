5b4a78926f3c1a8b20534e84333df55ac6a990ca
aurelianfrg <aurelianfrangin@gmail.com>
2025-10-23 11:38:03 +0200

    new base for sockets : single threaded non-blocking server with polling

--- src/client/client.c
+++ src/client/client.c
@@ -1,165 +1,124 @@
+/* client.c
+ *
+ * Simple interactive TCP client.
+ *
+ * Build: gcc -o client client.c
+ * Run:   ./client 127.0.0.1 12345
+ *
+ * Type lines and press Enter to send. Ctrl-D (EOF) to exit.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <errno.h>
 #include <string.h>
+#include <errno.h>
+#include <unistd.h>
 
-#include "client.h"
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
-static void init(void)
-{
-#ifdef WIN32
-   WSADATA wsa;
-   int err = WSAStartup(MAKEWORD(2, 2), &wsa);
-   if(err < 0)
-   {
-      puts("WSAStartup failed !");
-      exit(EXIT_FAILURE);
-   }
-#endif
-}
+#include <poll.h>
 
-static void end(void)
-{
-#ifdef WIN32
-   WSACleanup();
-#endif
-}
+#define BUF_SIZE 4096
 
-static void app(const char *address, const char *name)
-{
-   SOCKET sock = init_connection(address);
-   char buffer[BUF_SIZE];
+int main(int argc, char **argv) {
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <server-ip> <port>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
 
-   fd_set rdfs;
+    const char *server_ip = argv[1];
+    int port = atoi(argv[2]);
+    if (port <= 0 || port > 65535) {
+        fprintf(stderr, "Invalid port: %s\n", argv[2]);
+        return EXIT_FAILURE;
+    }
 
-   /* send our name */
-   write_server(sock, name);
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) { perror("socket"); return EXIT_FAILURE; }
 
-   while(1)
-   {
-      FD_ZERO(&rdfs);
+    struct sockaddr_in srv;
+    memset(&srv, 0, sizeof(srv));
+    srv.sin_family = AF_INET;
+    srv.sin_port = htons((unsigned short)port);
+    if (inet_pton(AF_INET, server_ip, &srv.sin_addr) != 1) {
+        fprintf(stderr, "inet_pton failed for %s\n", server_ip);
+        close(sock);
+        return EXIT_FAILURE;
+    }
 
-      /* add STDIN_FILENO */
-      FD_SET(STDIN_FILENO, &rdfs);
+    if (connect(sock, (struct sockaddr *)&srv, sizeof(srv)) < 0) {
+        perror("connect");
+        close(sock);
+        return EXIT_FAILURE;
+    }
 
-      /* add the socket */
-      FD_SET(sock, &rdfs);
+    printf("Connected to %s:%d. Type messages and press Enter.\n", server_ip, port);
 
-      if(select(sock + 1, &rdfs, NULL, NULL, NULL) == -1)
-      {
-         perror("select()");
-         exit(errno);
-      }
+    struct pollfd pfds[2];
+    pfds[0].fd = STDIN_FILENO;
+    pfds[0].events = POLLIN;
+    pfds[1].fd = sock;
+    pfds[1].events = POLLIN;
 
-      /* something from standard input : i.e keyboard */
-      if(FD_ISSET(STDIN_FILENO, &rdfs))
-      {
-         fgets(buffer, BUF_SIZE - 1, stdin);
-         {
-            char *p = NULL;
-            p = strstr(buffer, "\n");
-            if(p != NULL)
-            {
-               *p = 0;
-            }
-            else
-            {
-               /* fclean */
-               buffer[BUF_SIZE - 1] = 0;
-            }
-         }
-         write_server(sock, buffer);
-      }
-      else if(FD_ISSET(sock, &rdfs))
-      {
-         int n = read_server(sock, buffer);
-         /* server down */
-         if(n == 0)
-         {
-            printf("Server disconnected !\n");
+    char buf[BUF_SIZE];
+
+    while (1) {
+        int ready = poll(pfds, 2, -1);
+        if (ready < 0) {
+            if (errno == EINTR) continue;
+            perror("poll");
             break;
-         }
-         puts(buffer);
-      }
-   }
+        }
 
-   end_connection(sock);
-}
+        // stdin
+        if (pfds[0].revents & POLLIN) {
+            ssize_t r = read(STDIN_FILENO, buf, sizeof(buf));
+            if (r < 0) {
+                perror("read stdin");
+                break;
+            } else if (r == 0) {
+                // EOF from user (Ctrl-D)
+                printf("EOF on stdin, closing.\n");
+                break;
+            } else {
+                // send to server
+                ssize_t sent = 0;
+                while (sent < r) {
+                    ssize_t s = send(sock, buf + sent, r - sent, 0);
+                    if (s < 0) {
+                        perror("send");
+                        goto out;
+                    }
+                    sent += s;
+                }
+            }
+        }
 
-static int init_connection(const char *address)
-{
-   SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
-   SOCKADDR_IN sin = { 0 };
-   struct hostent *hostinfo;
+        // socket
+        if (pfds[1].revents & POLLIN) {
+            ssize_t r = recv(sock, buf, sizeof(buf)-1, 0);
+            if (r < 0) {
+                perror("recv");
+                break;
+            } else if (r == 0) {
+                printf("Server closed connection.\n");
+                break;
+            } else {
+                buf[r] = '\0';
+                printf("Server: %s", buf); // server may include newline
+            }
+        }
 
-   if(sock == INVALID_SOCKET)
-   {
-      perror("socket()");
-      exit(errno);
-   }
+        if (pfds[1].revents & (POLLHUP | POLLERR | POLLNVAL)) {
+            printf("Socket closed or error.\n");
+            break;
+        }
+    }
 
-   hostinfo = gethostbyname(address);
-   if (hostinfo == NULL)
-   {
-      fprintf (stderr, "Unknown host %s.\n", address);
-      exit(EXIT_FAILURE);
-   }
-
-   sin.sin_addr = *(IN_ADDR *) hostinfo->h_addr_list[0];
-   sin.sin_port = htons(PORT);
-   sin.sin_family = AF_INET;
-
-   if(connect(sock,(SOCKADDR *) &sin, sizeof(SOCKADDR)) == SOCKET_ERROR)
-   {
-      perror("connect()");
-      exit(errno);
-   }
-
-   return sock;
-}
-
-static void end_connection(int sock)
-{
-   closesocket(sock);
-}
-
-static int read_server(SOCKET sock, char *buffer)
-{
-   int n = 0;
-
-   if((n = recv(sock, buffer, BUF_SIZE - 1, 0)) < 0)
-   {
-      perror("recv()");
-      exit(errno);
-   }
-
-   buffer[n] = 0;
-
-   return n;
-}
-
-static void write_server(SOCKET sock, const char *buffer)
-{
-   if(send(sock, buffer, strlen(buffer), 0) < 0)
-   {
-      perror("send()");
-      exit(errno);
-   }
-}
-
-int main(int argc, char **argv)
-{
-   if(argc < 2)
-   {
-      printf("Usage : %s [address] [pseudo]\n", argv[0]);
-      return EXIT_FAILURE;
-   }
-
-   init();
-
-   app(argv[1], argv[2]);
-
-   end();
-
-   return EXIT_SUCCESS;
+out:
+    close(sock);
+    return EXIT_SUCCESS;
 }
--- src/server/server.c
+++ src/server/server.c
@@ -1,239 +1,212 @@
+/* server.c
+ *
+ * Simple multi-client echo server using poll().
+ *
+ * Build: gcc -o server server.c
+ * Run:   ./server 12345
+ *
+ * Author: example
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <errno.h>
 #include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <signal.h>
 
-#include "server.h"
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
-static void init(void)
-{
-#ifdef WIN32
-   WSADATA wsa;
-   int err = WSAStartup(MAKEWORD(2, 2), &wsa);
-   if(err < 0)
-   {
-      puts("WSAStartup failed !");
-      exit(EXIT_FAILURE);
-   }
-#endif
+#include <poll.h>
+#include <fcntl.h>
+
+#define BACKLOG 16
+#define MAX_CLIENTS 1024
+#define BUF_SIZE 4096
+
+static volatile sig_atomic_t keep_running = 1;
+
+void int_handler(int _) { (void)_; keep_running = 0; }
+
+int set_nonblocking(int fd) {
+    int flags = fcntl(fd, F_GETFL, 0);
+    if (flags < 0) return -1;
+    return fcntl(fd, F_SETFL, flags | O_NONBLOCK);
 }
 
-static void end(void)
-{
-#ifdef WIN32
-   WSACleanup();
-#endif
-}
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <port>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
 
-static void app(void)
-{
-   SOCKET sock = init_connection();
-   char buffer[BUF_SIZE];
-   /* the index for the array */
-   int actual = 0;
-   int max = sock;
-   /* an array for all clients */
-   Client clients[MAX_CLIENTS];
+    signal(SIGINT, int_handler);
+    signal(SIGTERM, int_handler);
 
-   fd_set rdfs;
+    int port = atoi(argv[1]);
+    if (port <= 0 || port > 65535) {
+        fprintf(stderr, "Invalid port: %s\n", argv[1]);
+        return EXIT_FAILURE;
+    }
 
-   while(1)
-   {
-      int i = 0;
-      FD_ZERO(&rdfs);
+    int listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (listen_fd < 0) {
+        perror("socket");
+        return EXIT_FAILURE;
+    }
 
-      /* add STDIN_FILENO */
-      FD_SET(STDIN_FILENO, &rdfs);
+    int opt = 1;
+    if (setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
+        perror("setsockopt");
+        close(listen_fd);
+        return EXIT_FAILURE;
+    }
 
-      /* add the connection socket */
-      FD_SET(sock, &rdfs);
+    struct sockaddr_in addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_port = htons((unsigned short)port);
 
-      /* add socket of each client */
-      for(i = 0; i < actual; i++)
-      {
-         FD_SET(clients[i].sock, &rdfs);
-      }
+    if (bind(listen_fd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+        perror("bind");
+        close(listen_fd);
+        return EXIT_FAILURE;
+    }
 
-      if(select(max + 1, &rdfs, NULL, NULL, NULL) == -1)
-      {
-         perror("select()");
-         exit(errno);
-      }
+    if (listen(listen_fd, BACKLOG) < 0) {
+        perror("listen");
+        close(listen_fd);
+        return EXIT_FAILURE;
+    }
 
-      /* something from standard input : i.e keyboard */
-      if(FD_ISSET(STDIN_FILENO, &rdfs))
-      {
-         /* stop process when type on keyboard */
-         break;
-      }
-      else if(FD_ISSET(sock, &rdfs))
-      {
-         /* new client */
-         SOCKADDR_IN csin = { 0 };
-         size_t sinsize = sizeof csin;
-         int csock = accept(sock, (SOCKADDR *)&csin, &sinsize);
-         if(csock == SOCKET_ERROR)
-         {
-            perror("accept()");
-            continue;
-         }
+    if (set_nonblocking(listen_fd) < 0) {
+        perror("set_nonblocking");
+        // not fatal; continue
+    }
 
-         /* after connecting the client sends its name */
-         if(read_client(csock, buffer) == -1)
-         {
-            /* disconnected */
-            continue;
-         }
+    struct pollfd *pfds = calloc(MAX_CLIENTS + 1, sizeof(struct pollfd));
+    if (!pfds) {
+        perror("calloc");
+        close(listen_fd);
+        return EXIT_FAILURE;
+    }
 
-         /* what is the new maximum fd ? */
-         max = csock > max ? csock : max;
+    pfds[0].fd = listen_fd;
+    pfds[0].events = POLLIN;
+    int nfds = 1; // number of used entries in pfds
 
-         FD_SET(csock, &rdfs);
+    printf("Server listening on port %d\n", port);
 
-         Client c = { csock };
-         strncpy(c.name, buffer, BUF_SIZE - 1);
-         clients[actual] = c;
-         actual++;
-      }
-      else
-      {
-         int i = 0;
-         for(i = 0; i < actual; i++)
-         {
-            /* a client is talking */
-            if(FD_ISSET(clients[i].sock, &rdfs))
-            {
-               Client client = clients[i];
-               int c = read_client(clients[i].sock, buffer);
-               printf("%s", buffer);
-               /* client disconnected */
-               if(c == 0)
-               {
-                  closesocket(clients[i].sock);
-                  remove_client(clients, i, &actual);
-                  strncpy(buffer, client.name, BUF_SIZE - 1);
-                  strncat(buffer, " disconnected !", BUF_SIZE - strlen(buffer) - 1);
-                  send_message_to_all_clients(clients, client, actual, buffer, 1);
-               }
-               else
-               {
-                  send_message_to_all_clients(clients, client, actual, buffer, 0);
-               }
-               break;
+    char buf[BUF_SIZE];
+
+    while (keep_running) {
+        int timeout_ms = 1000; // wakeup every second to check signal
+        int ready = poll(pfds, nfds, timeout_ms);
+        if (ready < 0) {
+            if (errno == EINTR) continue;
+            perror("poll");
+            break;
+        } else if (ready == 0) {
+            continue; // timeout, loop again to check keep_running
+        }
+
+        // Check listening socket
+        if (pfds[0].revents & POLLIN) {
+            // accept in a loop (because non-blocking)
+            while (1) {
+                struct sockaddr_in cli_addr;
+                socklen_t cli_len = sizeof(cli_addr);
+                int client_fd = accept(listen_fd, (struct sockaddr *)&cli_addr, &cli_len);
+                if (client_fd < 0) {
+                    if (errno == EAGAIN || errno == EWOULDBLOCK) break;
+                    perror("accept");
+                    break;
+                }
+
+                // find free slot
+                if (nfds >= MAX_CLIENTS + 1) {
+                    fprintf(stderr, "Too many connections, rejecting\n");
+                    close(client_fd);
+                    continue;
+                }
+
+                if (set_nonblocking(client_fd) < 0) {
+                    // not fatal
+                }
+
+                pfds[nfds].fd = client_fd;
+                pfds[nfds].events = POLLIN;
+                nfds++;
+
+                char ipbuf[INET_ADDRSTRLEN];
+                inet_ntop(AF_INET, &cli_addr.sin_addr, ipbuf, sizeof(ipbuf));
+                printf("Accepted %s:%d (fd=%d)\n", ipbuf, ntohs(cli_addr.sin_port), client_fd);
             }
-         }
-      }
-   }
+        }
 
-   clear_clients(clients, actual);
-   end_connection(sock);
-}
+        // Check client sockets (start at 1)
+        for (int i = 1; i < nfds; ++i) {
+            int fd = pfds[i].fd;
+            short re = pfds[i].revents;
+            if (re == 0) continue;
 
-static void clear_clients(Client *clients, int actual)
-{
-   int i = 0;
-   for(i = 0; i < actual; i++)
-   {
-      closesocket(clients[i].sock);
-   }
-}
+            if (re & (POLLERR | POLLHUP | POLLNVAL)) {
+                // client disconnected/error
+                close(fd);
+                // remove this pfds entry by shifting
+                pfds[i] = pfds[nfds - 1];
+                nfds--;
+                i--; // check the moved one on next iteration
+                continue;
+            }
 
-static void remove_client(Client *clients, int to_remove, int *actual)
-{
-   /* we remove the client in the array */
-   memmove(clients + to_remove, clients + to_remove + 1, (*actual - to_remove - 1) * sizeof(Client));
-   /* number client - 1 */
-   (*actual)--;
-}
+            if (re & POLLIN) {
+                ssize_t r = recv(fd, buf, sizeof(buf), 0);
+                if (r < 0) {
+                    if (errno == EAGAIN || errno == EWOULDBLOCK) continue;
+                    perror("recv");
+                    close(fd);
+                    pfds[i] = pfds[nfds - 1];
+                    nfds--;
+                    i--;
+                    continue;
+                } else if (r == 0) {
+                    // client closed
+                    printf("Client fd=%d closed\n", fd);
+                    close(fd);
+                    pfds[i] = pfds[nfds - 1];
+                    nfds--;
+                    i--;
+                    continue;
+                } else {
 
-static void send_message_to_all_clients(Client *clients, Client sender, int actual, const char *buffer, char from_server)
-{
-   int i = 0;
-   char message[BUF_SIZE];
-   message[0] = 0;
-   for(i = 0; i < actual; i++)
-   {
-      /* we don't send message to the sender */
-      if(sender.sock != clients[i].sock)
-      {
-         if(from_server == 0)
-         {
-            strncpy(message, sender.name, BUF_SIZE - 1);
-            strncat(message, " : ", sizeof message - strlen(message) - 1);
-         }
-         strncat(message, buffer, sizeof message - strlen(message) - 1);
-         write_client(clients[i].sock, message);
-      }
-   }
-}
+                    // standard case : process how you handle the client
+                    ssize_t sent = 0;
+                    while (sent < r) {
+                        ssize_t s = send(fd, buf + sent, r - sent, 0);
+                        if (s < 0) {
+                            if (errno == EAGAIN || errno == EWOULDBLOCK) continue;
+                            perror("send");
+                            break;
+                        }
+                        sent += s;
+                    }
+                    // For visibility, print what we got (trim newline)
+                    printf("fd=%d: %.*s\n", fd, (int)r, (int)r ? buf : "(empty)");
+                }
+            }
+        }
+    }
 
-static int init_connection(void)
-{
-   SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
-   SOCKADDR_IN sin = { 0 };
+    printf("Shutting down server...\n");
+    // Close all open fds
+    for (int i = 0; i < nfds; ++i) close(pfds[i].fd);
+    free(pfds);
+    close(listen_fd);
 
-   if(sock == INVALID_SOCKET)
-   {
-      perror("socket()");
-      exit(errno);
-   }
-
-   sin.sin_addr.s_addr = htonl(INADDR_ANY);
-   sin.sin_port = htons(PORT);
-   sin.sin_family = AF_INET;
-
-   if(bind(sock,(SOCKADDR *) &sin, sizeof sin) == SOCKET_ERROR)
-   {
-      perror("bind()");
-      exit(errno);
-   }
-
-   if(listen(sock, MAX_CLIENTS) == SOCKET_ERROR)
-   {
-      perror("listen()");
-      exit(errno);
-   }
-
-   return sock;
-}
-
-static void end_connection(int sock)
-{
-   closesocket(sock);
-}
-
-static int read_client(SOCKET sock, char *buffer)
-{
-   int n = 0;
-
-   if((n = recv(sock, buffer, BUF_SIZE - 1, 0)) < 0)
-   {
-      perror("recv()");
-      /* if recv error we disonnect the client */
-      n = 0;
-   }
-
-   buffer[n] = 0;
-
-   return n;
-}
-
-static void write_client(SOCKET sock, const char *buffer)
-{
-   if(send(sock, buffer, strlen(buffer), 0) < 0)
-   {
-      perror("send()");
-      exit(errno);
-   }
-}
-
-int main(int argc, char **argv)
-{
-   init();
-
-   app();
-
-   end();
-
-   return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
